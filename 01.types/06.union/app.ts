a6359b5837f43879023af85a9ee2461bca79ab82
Jonathan Manera <jonathan.manera@globallogic.com>
2020-05-29 21:30:44 -0300

    more types

---
+++ 01.types/06.union/app.ts
@@ -0,0 +1,9 @@
+function combine(input1: number | string, input2: number | string) {
+    return input1.toString() + input2
+}
+
+const comb1 = combine(4, 'ever')
+const comb2 = combine('Gr', 8)
+
+console.log(comb1)
+console.log(comb2)
---
+++ 01.types/08.custom-types/app.ts
@@ -0,0 +1,33 @@
+enum resultConversion {
+  AsNumber = 'number', AsString = 'string' 
+}
+
+type combinable = number | string
+
+type combined = {
+  ret: combinable,
+  as: resultConversion
+}
+
+function combine(
+    input1: combinable,
+    input2: combinable,
+    resultConv: resultConversion.AsNumber | resultConversion.AsString 
+  ) {
+    let result : combinable = ""
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConv === resultConversion.AsNumber) {
+      result = (+input1) + (+input2)
+    } else if (resultConv === resultConversion.AsString) {
+      result = input1.toString() + input2
+    }
+
+    let c: combined = {
+      ret: result,
+      as: resultConv
+    }
+    return c 
+  }
+
+  console.log(combine(30, 26, resultConversion.AsNumber))
+  
+  console.log(combine('Mary', 'Jane', resultConversion.AsString))
