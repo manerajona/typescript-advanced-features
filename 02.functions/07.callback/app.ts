81dd7cfb1aa971240f0285971063cf4a0f5d91a1
Jonathan Manera <jonathan.manera@globallogic.com>
2020-05-30 12:19:02 -0300

    callback

--- 02.functions/07.callback/app.ts
+++ 02.functions/07.callback/app.ts
@@ -10,16 +10,17 @@ function printResult(result: number): void {
 function addAndPrint(
   print: (n: number) => void,
   operation: (a: number, b: number) => number,
-  n1 : number,
-  n2 : number
+  n1: number,
+  n2: number
 ) {
   print(operation(n1, n2))
 }
 
 addAndPrint(printResult, add, 1, 2)
 
-addAndPrint((result:number)=>{console.log('result='+result)} , add, 2, 3)
+addAndPrint((result: number) => console.log('result=' + result), add, 2, 3)
 
 addAndPrint(
-  (result:number)=>{console.log('result='+result)}, 
-  (a: number, b: number) => a*b, 4 , 5)
+  (result: number) => console.log('result=' + result),
+  (a: number, b: number) => a * b,
+  4, 5)
