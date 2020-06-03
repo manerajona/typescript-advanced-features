d00a9009bbf87eea84c50566b64b2a3b73a08e8a
Jonathan Manera <jonathan.manera@globallogic.com>
2020-06-02 21:51:16 -0300

    decorators

--- 07.decorators/02.factory/app.ts
+++ 07.decorators/02.factory/app.ts
@@ -1,23 +1,23 @@
 function WithTemplate(template: string, hookId: string) {
     return function (constructor: any) {
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
+        const hookEl = document.getElementById(hookId)
+        const p = new constructor()
         if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1') !.textContent = p.name;
+            hookEl.innerHTML = template
+            hookEl.querySelector('h1') !.textContent = p.name
         }
     }
 }
 
 @WithTemplate('<h1></h1>', 'app')
 class Thing {
-    name = 'Hello!!';
+    name = 'Hello!!'
 
     constructor() {
-        console.log('Creating person object...');
+        console.log('Creating person object...')
     }
 }
 
-const pers = new Thing();
+const pers = new Thing()
 
-console.log(pers);
+console.log(pers)
